b979ed913558afd60363d45a240c7909140a186c
yashvant <yashvantyadav855@gmail.com>
2023-12-11 10:05:10 +0530

    Initial commit

---
+++ src/test/java/com/yashvant/cricketeer_backend/CricketeerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashvant.cricketeer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketeerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
